18c9cdd5c4f7de7269fb5084cdd77e21a352c7de
Salvador Gonzalez Nolasco <salvador.gonzalez-nolasco@capgemini.com>
2021-03-22 12:09:13 -0600

    Login page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, Router, RouterStateSnapshot} from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'mmbc-ng-front';
+  title = 'MMBC';
+
+  private cognito: string = "https://mmbc-application-group.auth.us-east-2.amazoncognito.com/login?client_id=14a5gt4i7ueqeh1l1kllo6d10h&response_type=code&scope=aws.cognito.signin.user.admin+email+openid+phone+profile&redirect_uri=https://development.d2itkv569wz6b6.amplifyapp.com/home";
+
+  onInit() {
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  mmbcBackendUrl: "http://ec2-3-16-135-225.us-east-2.compute.amazonaws.com:8080",
 };
 
 /*
